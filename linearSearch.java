07882bc85e3bf9b42e70e925e9c9195112055787
Uzair Hasan <uzairhasan12@hotmail.com>
2018-04-22 14:47:53 -0400

    Update linearSearch.java

--- linearSearch.java
+++ linearSearch.java
@@ -1,17 +1,20 @@
-
+//Insertion Sort By Uzair Hasan
+//This made this from scratch
 public class LinearSearchAlgorithm {
 	public static void main (String[] args) {
-		int arr [] = {0,1,2,3,4,5,6,7,8};
-		int here=linearSearch(arr,4);
-		System.out.println(here);
+		int A[] = {0,1,2,3,4,5,6,7,8}; //Random test array
+		int B = 4; //Random search value
+		int C = linearSearch(arr,B);
 	}
 	
-	public static int linearSearch(int arra[], int thiss) {
-		for(int i = 0; i<arra.length;i++) {
-			if(arra[i]==thiss) {
-				return i;
+	public static int linearSearch(int A[], int B) {
+		for(int i = 0; i<A.length;i++) { //Loop through all values in the array
+			if(A[i] == B) { //Check if the value at the current index is equal to the number we are looking for
+				return i; ///If equal, return index value
 			}
 		}
-		return -1;
+		return -1; //Return -1 (which can never be an index) if no match was found
 	}
 }
+//Any questions, feel free to email me and ask!
+//uzairhasan12@hotmail.com
